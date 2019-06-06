16f9eff2d98e39e81022f2b6372566fa256a9b11
AndGasper <24193043+AndGasper@users.noreply.github.com>
2019-06-05 19:22:31 -0700

    Added a silly count although there's no relationship between when the count.js file was run and when the records were added to the raw log file. Also, sensible chuckle on not thinking through the log format.

---
+++ count.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const FILE_TO_READ = path.resolve(__dirname, 'output', 'raw_container_output.log');
+const COUNT_STATS_FILE = path.resolve(__dirname, 'output', 'container_count_output.json');
+
+const containers = {};
+fs.readFile(FILE_TO_READ, { encoding: 'utf-8' }, function(error, data) {
+    // if no error, do stuff
+    if (!error) {
+        const explodedAtDelimiter = data.split('\n');
+        // shocked tom meme
+        const emptyStringFiltered = explodedAtDelimiter.filter( entry => (entry !== '') );
+        emptyStringFiltered.map(logEntry => {
+            const containerId = logEntry.split(' ')[1]; // ["containerId", "hash?"]
+            // if the container isn't listed then list it and increment the value
+            if (!containers[containerId]) {
+                containers[containerId] = 0;
+                containers[containerId]++;
+            } else {
+                // else the container is listed and increment the occurences
+                containers[containerId]++;
+            }
+        });
+        containers.timestamp = new Date();
+        fs.writeFileSync(COUNT_STATS_FILE, JSON.stringify(containers), {
+            encoding: 'utf-8',
+            flag: 'a'
+        });
+    }
+});
+
+
+// separate at delimiter then do logic
